679230b0eea1762e968462187a015d0d6e425d41
Obinna <obinna.achara@outlook.com>
2022-01-22 23:03:46 +0100

    Main Project

---
+++ Form1.Designer.vb
@@ -0,0 +1,157 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.calculate = New System.Windows.Forms.Button()
+        Me.Buttonexit = New System.Windows.Forms.Button()
+        Me.txtscores = New System.Windows.Forms.TextBox()
+        Me.lblresult = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ErrorProvider = New System.Windows.Forms.ErrorProvider(Me.components)
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        CType(Me.ErrorProvider, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.GroupBox2)
+        Me.GroupBox1.Controls.Add(Me.txtscores)
+        Me.GroupBox1.Controls.Add(Me.lblresult)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(31, 25)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(224, 207)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Button1)
+        Me.GroupBox2.Controls.Add(Me.calculate)
+        Me.GroupBox2.Controls.Add(Me.Buttonexit)
+        Me.GroupBox2.Location = New System.Drawing.Point(6, 142)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(212, 59)
+        Me.GroupBox2.TabIndex = 8
+        Me.GroupBox2.TabStop = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(83, 19)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(52, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "C&lear"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'calculate
+        '
+        Me.calculate.Location = New System.Drawing.Point(6, 19)
+        Me.calculate.Name = "calculate"
+        Me.calculate.Size = New System.Drawing.Size(71, 23)
+        Me.calculate.TabIndex = 1
+        Me.calculate.Text = "&Caliculate"
+        Me.calculate.UseVisualStyleBackColor = True
+        '
+        'Buttonexit
+        '
+        Me.Buttonexit.Location = New System.Drawing.Point(141, 19)
+        Me.Buttonexit.Name = "Buttonexit"
+        Me.Buttonexit.Size = New System.Drawing.Size(65, 23)
+        Me.Buttonexit.TabIndex = 3
+        Me.Buttonexit.Text = "&Exit"
+        Me.Buttonexit.UseVisualStyleBackColor = True
+        '
+        'txtscores
+        '
+        Me.txtscores.Location = New System.Drawing.Point(67, 31)
+        Me.txtscores.Name = "txtscores"
+        Me.txtscores.Size = New System.Drawing.Size(73, 20)
+        Me.txtscores.TabIndex = 0
+        '
+        'lblresult
+        '
+        Me.lblresult.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblresult.Location = New System.Drawing.Point(88, 82)
+        Me.lblresult.Name = "lblresult"
+        Me.lblresult.Size = New System.Drawing.Size(52, 21)
+        Me.lblresult.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(9, 90)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(43, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Result"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(6, 31)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(40, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Score"
+        '
+        'ErrorProvider
+        '
+        Me.ErrorProvider.ContainerControl = Me
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.DarkRed
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Student Scores"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        CType(Me.ErrorProvider, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents calculate As System.Windows.Forms.Button
+    Friend WithEvents Buttonexit As System.Windows.Forms.Button
+    Friend WithEvents txtscores As System.Windows.Forms.TextBox
+    Friend WithEvents lblresult As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents ErrorProvider As System.Windows.Forms.ErrorProvider
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,89 @@
+﻿Public Class Form1
+
+    Private Sub Buttonexit_Click(sender As Object, e As EventArgs) Handles Buttonexit.Click
+
+        'to close the app
+
+        Dim returndialogresult As System.Windows.Forms.DialogResult
+        Dim title As String = "Sure to exit"
+        Dim message As String = "Do you want to exit?"
+
+        returndialogresult = MessageBox.Show(message, title, MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2)
+
+        If returndialogresult = Windows.Forms.DialogResult.Yes Then
+
+            Me.Close()
+        End If
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        txtscores.Text = ""
+        lblresult.Text = ""
+
+
+    End Sub
+
+    Private Sub txtscores_TextChanged(sender As Object, e As EventArgs) Handles txtscores.TextChanged
+
+    End Sub
+
+    Private Sub txtscores_Validating(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles txtscores.Validating
+
+        Dim score As Integer
+
+        Try
+            score = Integer.Parse(txtscores.Text)
+
+            If txtscores.Text = String.Empty Then
+
+                'Cancel the event
+                e.Cancel = True
+                txtscores.SelectAll()
+                txtscores.Focus()
+
+                ErrorProvider.SetError(txtscores, "Score must not be blank")
+
+            End If
+
+        Catch ex As Exception
+            e.Cancel = True
+            txtscores.SelectAll()
+            txtscores.Focus()
+            ErrorProvider.SetError(txtscores, "Year must be numeric")
+        End Try
+
+    End Sub
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+
+        e.Cancel = False
+
+    End Sub
+
+   
+    Private Sub calculate_Click(sender As Object, e As EventArgs) Handles calculate.Click
+
+        Dim scores As Integer
+
+        scores = Val(txtscores.Text)
+
+        If scores >= 70 Then
+            lblresult.Text = "A"
+        ElseIf scores >= 60 Then
+            lblresult.Text = "B"
+        ElseIf scores >= 50 Then
+            lblresult.Text = "C"
+        ElseIf scores >= 40 Then
+            lblresult.Text = "D"
+        Else
+            lblresult.Text = "F"
+        End If
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
